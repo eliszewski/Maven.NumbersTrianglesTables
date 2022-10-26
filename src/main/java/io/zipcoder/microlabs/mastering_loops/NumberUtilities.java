bc7ad4e019030f4007f565857ba6927d5a3922bf
Eryk <eryk@Zipcoders-MacBook-Pro-2.local>
2022-10-26 14:56:57 -0400

    solved some problems

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,12 +3,25 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String evenNumbers = "";
+        for(int i = start; i < stop; i++){
+            if(i % 2 == 0){
+                evenNumbers += i;
+                //evenNumbers.concat(Integer.toString(i));
+            }
+        }
+        return evenNumbers;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String oddNumbers = "";
+        for(int i = start; i < stop; i++){
+            if(i % 2 == 1){
+                oddNumbers += i;
+            }
+        }
+        return oddNumbers;
     }
 
 
@@ -17,11 +30,18 @@ public class NumberUtilities {
     }
 
     public static String getRange(int start) {
+        //String range = "";
+        //for(int i =0; i <)
         return null;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String range = "" ;
+        for(int i = start; i < stop; i++){
+            range += i;
+        }
+
+        return range;
     }
 
 
